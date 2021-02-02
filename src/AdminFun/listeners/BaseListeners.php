f29affe5db2967696d5250661a7eb0375a03e1de
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-02-01 17:36:50 -0800

    Update BaseListeners.php

--- src/AdminFun/listeners/BaseListeners.php
+++ src/AdminFun/listeners/BaseListeners.php
@@ -22,15 +22,15 @@ namespace AdminFun\listeners;
 use AdminFun\AdminFun;
 use pocketmine\event\Listener;
 
-abstract class BaseListeners implements Listener{
+abstract class BaseListeners implements Listener {
 
 	private $plugin;
 
-	public function __construct(AdminFun $plugin){
+	public function __construct(AdminFun $plugin) {
 		$this->plugin = $plugin;
 	}
 
-	public final function getPlugin(){
+	public final function getPlugin() {
 		return $this->plugin;
 	}
 
